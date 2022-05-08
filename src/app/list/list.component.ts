cca5684555549c93497f95d4cc9c0c9053985267
adlerjani <adlerphotobusiness@gmail.com>
2022-05-09 00:43:13 +0200

    table component done

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Film {
+  name: string;
+  release: string;
+  director: string;
+  actors: string;
+}
+
+const FilmList: Film[] = [
+  {
+    name: 'A keresztapa',
+    release: '1972',
+    director: 'Francis Ford Coppola',
+    actors: 'Al Pacino, Diane Keaton, Marlon Brando, Robert Duvall, James Caan, Talia Shire, John Cazale, Sofia Coppola'
+  },
+  
+];
+
+@Component({
+  selector: 'mm-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+
+  /* constructor() { }
+
+  ngOnInit(): void {
+  } */
+
+  films = FilmList;
+
+}
